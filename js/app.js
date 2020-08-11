07b01816d98f58649d77ef58a134ab0800196fe3
LeeThomas13 <lmtthomasritz@gmail.com>
2020-08-10 18:04:44 -0700

    added variables to js, ultimately testing branch flow

---
+++ js/app.js
@@ -0,0 +1,29 @@
+var seattle = {
+  minCustomer = 23,
+  maxCustomer = 65,
+  avgCookieSale = 6.3,
+}
+
+var tokyo = {
+  minCustomer = 3,
+  maxCustomer = 24,
+  avgCookieSale = 1.2,
+}
+
+var dubai = {
+  minCustomer = 11,
+  maxCustomer = 38,
+  avgCookieSale = 3.7,
+}
+
+var paris = {
+  minCustomer = 20,
+  maxCustomer = 38,
+  avgCookieSale = 2.3,
+}
+
+var lima = {
+  minCustomer = 2,
+  maxCustomer = 16,
+  avgCookieSale = 4.6,
+}
