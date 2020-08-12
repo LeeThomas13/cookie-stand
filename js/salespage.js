429dd52ab7e1b23ba745ef78d8564c9333c1717f
LeeThomas13 <lmtthomasritz@gmail.com>
2020-08-11 18:05:48 -0700

    we got the methods working, now time to add and manipulate them to the DOM

--- js/salespage.js
+++ js/salespage.js
@@ -1,33 +1,137 @@
-'Use Strict'
+'use strict'
 
-// proof of life
-console.log('IM ALIVE');
+//proof of life
+console.log('ITS ALIVE!')
 
-var storeHourArr = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'];
+var storeHourArr = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
-var seattle = {
-  minCustomer = 23,
-  maxCustomer = 65,
-  avgCookieSale = 6.3,
-  customersEachHour: [],
-  cookiesSoldEachHour: [],
+// I got this from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
-  calculateCustomersEachHour: function () {
-    for(var i=0; i < storeHourArr.length; i++);
-    var customers = getRandomNumber(this.minCustomer,this.maxCustomer);
-    this.customersEachHour.push(customer);
-  }
-
-  // calculateCookiesEachHour: function () {
-  //   for(var i=0; i<)
-  // }
-}
-
-seattle.calculateCustomersEachHour();
-console.log(seattle)
-
-//I got this from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-
-function getRandomNumber(min,max) {
+function getRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+var seattle = {
+  minCustomer: 23,
+  maxCustomer: 65,
+  avgCookieSale: 6.3,
+  customersEachHour: [],
+  cookiesSoldPerHour: [],
+  totalCookiesSoldToday: 0,
+
+  calculateCustomersEachHour: function () {
+    for (var i = 0; i < storeHourArr.length; i++) {
+      var customers = getRandomNumber(this.minCustomer, this.maxCustomer);
+      this.customersEachHour.push(customers);
+    }
+  },
+
+  calculateCookiesEachHour: function () {
+    for (var i = 0; i < this.customersEachHour.length; i++) {
+      var cookies = Math.ceil(this.customersEachHour[i] * this.avgCookieSale);
+      this.cookiesSoldPerHour.push(cookies);
+      this.totalCookiesSoldToday = this.totalCookiesSoldToday + this.cookiesSoldPerHour[i];
+    }
+  },
+} 
+
+var tokyo = {
+  minCustomer: 3,
+  maxCustomer: 24,
+  avgCookieSale: 1.2,
+  customersEachHour: [],
+  cookiesSoldPerHour: [],
+  totalCookiesSoldToday: 0,
+
+  calculateCustomersEachHour: function () {
+    for (var i = 0; i < storeHourArr.length; i++) {
+      var customers = getRandomNumber(this.minCustomer, this.maxCustomer);
+      this.customersEachHour.push(customers);
+    }
+  },
+
+  calculateCookiesEachHour: function () {
+    for (var i = 0; i < this.customersEachHour.length; i++) {
+      var cookies = Math.ceil(this.customersEachHour[i] * this.avgCookieSale);
+      this.cookiesSoldPerHour.push(cookies);
+      this.totalCookiesSoldToday = this.totalCookiesSoldToday + this.cookiesSoldPerHour[i];
+    }
+  },
+} 
+
+var dubai = {
+  minCustomer: 11,
+  maxCustomer: 38,
+  avgCookieSale: 3.7,
+  customersEachHour: [],
+  cookiesSoldPerHour: [],
+  totalCookiesSoldToday: 0,
+
+  calculateCustomersEachHour: function () {
+    for (var i = 0; i < storeHourArr.length; i++) {
+      var customers = getRandomNumber(this.minCustomer, this.maxCustomer);
+      this.customersEachHour.push(customers);
+    }
+  },
+
+  calculateCookiesEachHour: function () {
+    for (var i = 0; i < this.customersEachHour.length; i++) {
+      var cookies = Math.ceil(this.customersEachHour[i] * this.avgCookieSale);
+      this.cookiesSoldPerHour.push(cookies);
+      this.totalCookiesSoldToday = this.totalCookiesSoldToday + this.cookiesSoldPerHour[i];
+    }
+  },
+} 
+
+var paris = {
+  minCustomer: 20,
+  maxCustomer: 38,
+  avgCookieSale: 2.3,
+  customersEachHour: [],
+  cookiesSoldPerHour: [],
+  totalCookiesSoldToday: 0,
+
+  calculateCustomersEachHour: function () {
+    for (var i = 0; i < storeHourArr.length; i++) {
+      var customers = getRandomNumber(this.minCustomer, this.maxCustomer);
+      this.customersEachHour.push(customers);
+    }
+  },
+
+  calculateCookiesEachHour: function () {
+    for (var i = 0; i < this.customersEachHour.length; i++) {
+      var cookies = Math.ceil(this.customersEachHour[i] * this.avgCookieSale);
+      this.cookiesSoldPerHour.push(cookies);
+      this.totalCookiesSoldToday = this.totalCookiesSoldToday + this.cookiesSoldPerHour[i];
+    }
+  },
+} 
+
+var lima = {
+  minCustomer: 2,
+  maxCustomer: 16,
+  avgCookieSale: 4.6,
+  customersEachHour: [],
+  cookiesSoldPerHour: [],
+  totalCookiesSoldToday: 0,
+
+  calculateCustomersEachHour: function () {
+    for (var i = 0; i < storeHourArr.length; i++) {
+      var customers = getRandomNumber(this.minCustomer, this.maxCustomer);
+      this.customersEachHour.push(customers);
+    }
+  },
+
+  calculateCookiesEachHour: function () {
+    for (var i = 0; i < this.customersEachHour.length; i++) {
+      var cookies = Math.ceil(this.customersEachHour[i] * this.avgCookieSale);
+      this.cookiesSoldPerHour.push(cookies);
+      this.totalCookiesSoldToday = this.totalCookiesSoldToday + this.cookiesSoldPerHour[i];
+    }
+  },
+} 
+
+
+
+
+
