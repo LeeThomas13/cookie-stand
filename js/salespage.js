108bc5e9fedf073f42361097ea4baef650d39966
LeeThomas13 <lmtthomasritz@gmail.com>
2020-08-12 21:33:37 -0700

    finished js, cant get it to print to a webpage

--- js/salespage.js
+++ js/salespage.js
@@ -2,6 +2,12 @@
 var allStores = [];
 var storeHourArr = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 var parentElement = document.getElementById('table');
+var seattle = new StoreLocation('Seattle', 23, 65, 6.3)
+var tokyo = new StoreLocation('Tokyo', 3, 24, 1.2)
+var dubai = new StoreLocation('Dubai', 11, 38, 3.7)
+var paris = new StoreLocation('Paris', 20, 38, 2.3)
+var lima = new StoreLocation('Lima', 2, 16, 4.6)
+
 
 function StoreLocation(name, minCustomer, maxCustomer, avgCookieSale,) {
   this.storeName = name;
@@ -43,13 +49,6 @@ StoreLocation.prototype.render = function () {
   parentElement.appendChild(tableRow);
 }
 
-
-var seattle = new StoreLocation('Seattle', 23, 65, 6.3)
-var tokyo = new StoreLocation('Tokyo', 3, 24, 1.2)
-var dubai = new StoreLocation('Dubai', 11, 38, 3.7)
-var paris = new StoreLocation('Paris', 20, 38, 2.3)
-var lima = new StoreLocation('Lima', 2, 16, 4.6)
-
 function generateHeader(){
   var tableRow = document.createElement('tr')
   for (var i=0;i<storeHourArr.length; i++){
@@ -74,6 +73,9 @@ function generateContent(){
   }
 }
 
+// calculateCustomersEachHour()
+// calculateCookiesSoldPerHour()
+// StoreLocation()
 generateHeader()
 generateContent()
 
